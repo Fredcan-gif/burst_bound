419c7b5910716297e2504f56a3985b7522f23f62
Fredcan-gif <fredrikcanon@gmail.com>
2026-05-01 21:39:30 +0800

    Updated tutorial

--- reset_zone.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@export var spawn_point: Vector2 = Vector2.ZERO
-
-func _on_body_entered(body):
-	if body.is_in_group("Player"):
-		body.global_position = spawn_point
-		body.velocity = Vector2.ZERO
